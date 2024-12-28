188044addd54c20f4abc7b40b48f5d945de92568
decker <deckerurbano@gmail.com>
2024-12-28 16:27:46 -0300

    feat(database): add schema for rooms, messages, and index
    Introduced `rooms` and `messages` tables with appropriate columns and relations. Added a composite primary key for the `messages_index`. This update structures the data model for handling chat functionality efficiently.

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -1,6 +1,38 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  primaryKey,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+export const rooms = pgTable("rooms", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
 });
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  roomId: integer("room_id")
+    .references(() => rooms.id)
+    .notNull(),
+  username: varchar("username").notNull(),
+  content: varchar("content").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export const messagesIndex = pgTable(
+  "messages_index",
+  {
+    roomId: integer("room_id").notNull(),
+    createdAt: timestamp("created_at").notNull(),
+  },
+  (table) => ({
+    pk: primaryKey({
+      columns: [table.roomId, table.createdAt],
+    }),
+  }),
+);
