95b1ca391c4fa36c15f94736216f029e161ceb46
decker <deckerurbano@gmail.com>
2024-12-29 16:24:22 -0300

    chore: update scripts and improve database initialization
    Updated dev and build scripts to include automatic database migrations. Adjusted the database initialization logic to use VERCEL_ENV instead of NODE_ENV for better environment-specific configurations.

--- apps/web/database/index.ts
+++ apps/web/database/index.ts
@@ -6,13 +6,13 @@ import { env } from "@/env";
 import * as schema from "./schema";
 
 declare global {
-  // eslint-disable-next-line no-var -- only var works here
   var db: PostgresJsDatabase<typeof schema> | undefined;
 }
 
+// biome-ignore lint/suspicious/noRedeclare: <explanation>
 let db: PostgresJsDatabase<typeof schema>;
 
-if (env.NODE_ENV === "production") {
+if (env.VERCEL_ENV === "production") {
   db = drizzle(postgres(env.DATABASE_URL), { schema });
 } else {
   if (!global.db) global.db = drizzle(postgres(env.DATABASE_URL), { schema });
